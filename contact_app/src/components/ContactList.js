8f187b126715ef2051441f6b6667193a99e70f0f
Aniket Shinde <shindeaniket2630@gmail.com>
2025-04-03 23:10:11 +0530

    Fixed ContactList.js syntax errors and other improvements

---
+++ contact_app/src/components/ContactList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ContactList = (props) => {
+  console.log(props);
+
+  const rendercontactList = props.contacts.map((contact) => {
+    return (
+      <div className="item" key={contact.id}>
+        <div className="content">
+          <div className="header">{contact.name}</div>
+          <div>{contact.email}</div>
+        </div>
+        <i className="trash alternate outline icon"></i>
+      </div>
+    );
+  });
+
+  return <div className="ui celled list">{rendercontactList}</div>;
+};
+
+export default ContactList;
